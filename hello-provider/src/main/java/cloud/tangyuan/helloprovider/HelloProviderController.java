c8e9c605b22b504bb9f850704d358298ad183934
EGG-Gerome <2730833013@qq.com>
2025-12-14 21:12:43 +0800

    修改 hello-provider 的 Controller 类展示 调用实例的 port 来测试 Cloud Load Balance 的默认策略轮循

--- hello-provider/src/main/java/cloud/tangyuan/helloprovider/HelloProviderController.java
+++ hello-provider/src/main/java/cloud/tangyuan/helloprovider/HelloProviderController.java
@@ -3,14 +3,34 @@ package cloud.tangyuan.helloprovider;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+
+//@RestController
+//public class HelloProviderController {
+//    // 关键点1：多路径用{}包裹（数组语法），显式指定value属性（也可省略value=，但{}必须有）
+//    @GetMapping(value = {"/greet", "/greet/{username}"})
+//    // 关键点2：设置required=false，允许username为null
+//    public String  greet(@PathVariable(required = false) String username){
+//        String name = (username == null ? "tourist" : username);
+//        return "Hello " + name;
+//    }
+//}
 
 @RestController
 public class HelloProviderController {
-    // 关键点1：多路径用{}包裹（数组语法），显式指定value属性（也可省略value=，但{}必须有）
-    @GetMapping(value = {"/greet", "/greet/{username}"})
-    // 关键点2：设置required=false，允许username为null
-    public String  greet(@PathVariable(required = false) String username){
-        String name = (username == null ? "tourist" : username);
-        return "Hello " + name;
+    // 把 servicePort 变量与 server.port 配置属性绑定
+    @Value("${server.port}")
+    private String servicePort;
+
+    // 把 serviceName 变量与 spring.application.name 配置属性绑定
+    @Value("${spring.application.name}")
+    private String serviceName;
+
+    @GetMapping("/greet/{username}")
+    public String greet(@PathVariable("username") String username){
+        String msg = null;
+        msg = "Hello, %s <br>Service Name: %s <br>Service Port: %s".formatted(username, serviceName, servicePort);
+        return msg;
     }
+
 }
