a01301176ac59792b851200e72ed39659c5ec91b
EGG-Gerome <2730833013@qq.com>
2025-12-15 21:39:05 +0800

    通过三种方式来实现通过 RestTemplate 调用微服务（DiscoveryClient,LoadBalancerClient @LoadBalanced）

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # 使用的技术栈
-Spring Boot + Spring Cloud + Spring Cloud Alibaba + Nacos（服务注册 / 配置中心） + OpenFeign（远程调用） + Spring Cloud Bootstrap（配置引导）
+Spring Boot + Spring Cloud + Spring Cloud Alibaba + Nacos（服务注册 / 配置中心） + OpenFeign（远程调用） + Spring Cloud Bootstrap（配置引导）+ Cloud LoadBalancer
--- hello-provider/src/main/java/cloud/tangyuan/helloprovider/HelloProviderApplication.java
+++ hello-provider/src/main/java/cloud/tangyuan/helloprovider/HelloProviderApplication.java
@@ -2,8 +2,10 @@ package cloud.tangyuan.helloprovider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
 public class HelloProviderApplication {
 
 	public static void main(String[] args) {
